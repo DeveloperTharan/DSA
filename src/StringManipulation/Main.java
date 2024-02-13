034ad5d8a351ca5ac40bb924f2ee7af53b86d6c7
Dharanitharan <dharanitharanps@gmail.com>
2024-02-13 22:05:59 +0000

    string manipulation

---
+++ src/StringManipulation/Main.java
@@ -0,0 +1,8 @@
+package StringManipulation;
+
+public class Main {
+    public static void main(String[] args) {
+        var res = StringUtils.isPalindrome("Madam");
+        System.out.println(res);
+    }
+}
