9b0f7c6db5cebfba421dd71c6ca7716a57d148b2
hp <dharanitharanps@gmail.com>
2024-04-20 23:45:39 +0530

    MergeSort

--- src/Algorithms/sorting/Main.java
+++ src/Algorithms/sorting/Main.java
@@ -4,9 +4,36 @@ public class Main {
     public static void main(String[] args) {
         int[] data = { 9, 5, 3, 7, 2, 1 };
 
-        Sorting sort = new Sorting();
+        var bubledsort = Sorting.BubleSort(data);
+        var selectionsort = Sorting.SelectionSort(data);
+        var insertionSort = Sorting.InsertionSort(data);
+        var mergSort = Sorting.MergeSort(data);
 
-        System.out.println(sort.BubleSort(data));
-        System.out.println(sort.SelectionSort(data));
+        System.out.print("BubbledSort: ");
+        for (var nums : bubledsort) {
+            System.out.print(nums + " ");
+        }
+
+        System.out.println();
+
+        System.out.print("SelectionSort: ");
+        for (var nums : selectionsort) {
+            System.out.print(nums + " ");
+        }
+
+        System.out.println();
+
+        System.out.print("InsertionSort: ");
+        for (var nums : insertionSort) {
+            System.out.print(nums + " ");
+        }
+
+        
+        System.out.println();
+
+        System.out.print("MergeSort: ");
+        for (var nums : mergSort) {
+            System.out.print(nums + " ");
+        }
     }
 }
--- src/Algorithms/sorting/Sorting.java
+++ src/Algorithms/sorting/Sorting.java
@@ -2,47 +2,124 @@ package Algorithms.sorting;
 
 public class Sorting {
     /* Bubble sort */
-    public int[] BubleSort(int[] array) {
-        int size = array.length;
+    public static int[] BubleSort(int[] arr) {
+        int size = arr.length;
         int temp;
 
-        if (size <= 0) {
-            throw new IllegalArgumentException();
+        if (arr == null || size <= 1) {
+            return arr;
         }
 
         for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-        return array;
+        return arr;
     }
 
     /* Selection sort */
-    public int[] SelectionSort(int[] array) {
-        int size = array.length;
+    public static int[] SelectionSort(int[] arr) {
+        int size = arr.length;
         int temp;
         int minIndex = -1;
 
-        if (size <= 0) {
-            throw new IllegalArgumentException();
+        if (arr == null || size <= 1) {
+            return arr;
         }
 
-        for(int i=0; i<size-1; i++){
+        for (int i = 0; i < size - 1; i++) {
             minIndex = i;
-            for(int j=i+1; j<size; j++){
-                if(array[minIndex] > array[j]){
+            for (int j = i + 1; j < size; j++) {
+                if (arr[minIndex] > arr[j]) {
                     minIndex = j;
                 }
             }
-            temp = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = temp;
+            temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
-        return array;
+        return arr;
     }
+
+    /* insertionSort */
+    public static int[] InsertionSort(int[] arr) {
+        int size = arr.length;
+
+        if (arr == null || size <= 1) {
+            return arr;
+        }
+
+        for (int i = 1; i < size; ++i) {
+            int currentValue = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > currentValue) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = currentValue;
+        }
+        return arr;
+    }
+
+    /* MergeSort */
+    public static int[] MergeSort(int[] arr) {
+        int size = arr.length;
+        
+        if (arr == null || size <= 1){
+            return arr;
+        }
+
+        int midIndex = size / 2;
+
+        int[] left = new int[midIndex];
+        int[] right = new int[size - midIndex];
+
+        for (int i = 0; i < midIndex; i++) {
+            left[i] = arr[i];
+        }
+
+        for (int i = midIndex; i < size; i++) {
+            right[i - midIndex] = arr[i];
+        }
+
+        left = MergeSort(left);
+        right = MergeSort(right);
+
+        return Merge(left, right);
+    }
+
+    private static int[] Merge(int[] left, int[] right) {
+        int leftSize = left.length;
+        int rightSize = right.length;
+
+        int[] mergedArray = new int[leftSize + rightSize];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftSize && j < rightSize) {
+            if (left[i] <= right[j]) {
+                mergedArray[k++] = left[i++];
+            } else {
+                mergedArray[k++] = right[j++];
+            }
+        }
+
+        while (i < leftSize) {
+            mergedArray[k++] = left[i++];
+        }
+
+        while (j < rightSize) {
+            mergedArray[k++] = right[j++];
+        }
+
+        return mergedArray;
+    }
+
+    /* QuickSort */
 }
